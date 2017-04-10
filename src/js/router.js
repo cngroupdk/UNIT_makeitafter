26c2309deabcb3252bc188b31748bb42e2145e74
Matěj Polák <matejpolak42@gmail.com>
2017-04-10 21:45:41 +0200

    router - detect modifiers

--- src/js/router.js
+++ src/js/router.js
@@ -6,6 +6,9 @@ $(() => {
     $('.route')
         .hide()
         .on('click', 'a', function(e) {
+            if (e.altKey || e.shiftKey || e.ctrlKey) {
+                return; // let the browser handle this
+            }
             $.router.go($(this).attr('href'));
             e.preventDefault();
         })
