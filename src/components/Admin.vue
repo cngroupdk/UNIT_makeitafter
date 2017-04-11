7bf2250483293600859d1a7657aa7faba1c09201
Dejmas <dejmektomas@gmail.com>
2017-04-11 14:49:00 +0200

    tag-input component 2

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="admin">
         <h1>{{ box.name }}</h1>
-        <div class="btn-group" role="group" aria-label="Sorting">
-            <button type="button" class="btn btn-secondary" v-on:click="sort('dateTime')">Sort by Date</button>
-            <button type="button" class="btn btn-secondary" v-on:click="sort('text')">Sort by Popularity</button>
-        </div>
-        <div class="container">
+        <div class="col-sm-6">
+            <div class="btn-group" role="group" aria-label="Sorting">
+                <button type="button" class="btn btn-secondary" v-on:click="sort('dateTime')">Sort by Date</button>
+                <button type="button" class="btn btn-secondary" v-on:click="sort('text')">Sort by Popularity</button>
+            </div>
             <input-tag placeholder="Add Tag" :tags="tags" validate="text"></input-tag>
         </div>
+        <div class="clearfix"></div>
         <item v-for="item of items" :text="item.text" :dateTime="item.dateTime" :key="item.guid" />
     </div>
 </template>
@@ -21,17 +22,14 @@
             items: Array,
         },
         components : {
-            'input-tag': InputTag
+            'input-tag': InputTag,
+            item: require('./AdminItem'),
         },
 
         data () {
             return {
-                tags: ["helpdeskissue"]
+                tags: ["A", "SDF"]
             };
-        },
-
-        components: {
-            item: require('./AdminItem'),
         },
 
         methods: {
