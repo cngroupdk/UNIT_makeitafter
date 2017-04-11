64b7342a93bdeb0dbcade03fa17fcfd03367b9ed
Matěj Polák <matejpolak42@gmail.com>
2017-04-11 12:25:58 +0200

    fix merge

---
+++ src/js/Api.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+
+export default {
+
+    baseUrl: 'http://myslenkynezastavis.cz/api/',
+
+    request(method, resource, data) {
+        return $.ajax(this.baseUrl + resource, {
+            method,
+            data,
+        });
+    },
+
+
+    categories() {
+        return this.request('get', 'category');
+    },
+
+    boxes() {
+        return this.request('get', 'suggestion-box').then(response => response.boxes);
+    },
+
+    box(url) {
+        return this.request('get', 'suggestion-box').then(data => data.boxes.filter(i => i.url === url)[0]);
+    },
+
+    addBox(data) {
+        return this.request('post', 'suggestion-box', data);
+    },
+
+    items(box) {
+        return this.request('get', 'suggestion-item').then(data => data.items.filter(i => i.box === box || 1));
+    }
+
+}
