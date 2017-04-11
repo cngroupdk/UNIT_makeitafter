cc6c6edb1eb4d144775311300fbe7eac3efc6539
Matěj Polák <matejpolak42@gmail.com>
2017-04-11 13:09:10 +0200

    formatted SugestionBox

--- src/components/SuggestionBox.vue
+++ src/components/SuggestionBox.vue
@@ -1,41 +1,43 @@
 <template>
-  <div class="container">
-      <div v-if="showLoading" class="row">
-          <div class="alert alert-info">
-              Saving...
-          </div>
-      </div>
-      <div v-if="showThanks">
-          <div class="alert alert-success">
-              Thank you!
-          </div>
-      </div>
-	<div v-show="!showLoading && !showThanks" class="row" style="margin-top:40px;">
-		<div class="col-md-6">
-    	<div class="well well-sm">
-            <div class="text-right">
-                <a class="btn btn-success btn-green" href="#reviews-anchor" id="open-review-box">Leave a Review</a>
+    <div class="container">
+        <div v-if="showLoading" class="row">
+            <div class="alert alert-info">
+                Saving...
             </div>
-        
-            <div class="row" id="post-review-box" style="display:none;">
-                <div class="col-md-12">
-                    <input id="ratings-hidden" name="rating" type="hidden">
-                    <textarea class="form-control animated" cols="50" id="new-review" v-model="text" placeholder="Enter your review here..." rows="5"></textarea>
-
+        </div>
+        <div v-if="showThanks">
+            <div class="alert alert-success">
+                Thank you!
+            </div>
+        </div>
+        <div v-show="!showLoading && !showThanks" class="row" style="margin-top:40px;">
+            <div class="col-md-6">
+                <div class="well well-sm">
                     <div class="text-right">
-                        <div class="stars starrr" data-rating="0"></div>
-                        <a class="btn btn-danger btn-sm" href="#" id="close-review-box" style="display:none; margin-right: 10px;">
-                        <span class="glyphicon glyphicon-remove"></span>Cancel</a>
-                        <button class="btn btn-success btn-lg" type="submit" @click="save">Save</button>
+                        <a class="btn btn-success btn-green" href="#reviews-anchor"
+                           id="open-review-box">Leave a Review</a>
+                    </div>
+
+                    <div class="row" id="post-review-box" style="display:none;">
+                        <div class="col-md-12">
+                            <input id="ratings-hidden" name="rating" type="hidden">
+                            <textarea class="form-control animated" cols="50" id="new-review" v-model="text"
+                                      placeholder="Enter your review here..." rows="5"></textarea>
+
+                            <div class="text-right">
+                                <div class="stars starrr" data-rating="0"></div>
+                                <a class="btn btn-danger btn-sm" href="#" id="close-review-box"
+                                   style="display:none; margin-right: 10px;">
+                                    <span class="glyphicon glyphicon-remove"></span>Cancel</a>
+                                <button class="btn btn-success btn-lg" type="submit" @click="save">Save</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div> 
-         
-		</div>
-	</div>
-</div>
 
+            </div>
+        </div>
+    </div>
 
 
 </template>
