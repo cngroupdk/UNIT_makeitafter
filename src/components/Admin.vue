d927d94beb8f1706e966ad75745fc91c228d9573
Dejmas <dejmektomas@gmail.com>
2017-04-11 13:40:13 +0200

    sorting and filtering not done

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="admin">
         <h1>{{ box.name }}</h1>
+        <div class="btn-group" role="group" aria-label="Sorting">
+            <button type="button" class="btn btn-secondary" v-on:click="sort('dateTime')">Sort by Date</button>
+            <button type="button" class="btn btn-secondary" v-on:click="sort('text')">Sort by Popularity</button>
+        </div>
+        <div class="container">
+            <button v-for="tag in tags" v-show="tag.active" v-on:click="">{{tag.name}}</button>
+        </div>
         <div class="panel panel-primary" v-for="item of items">
             <div class="panel-body">
                 {{ item.text }}
@@ -18,7 +25,36 @@
         },
 
         data () {
-            return {};
+            return {
+                tags: [{
+                        name: "critical",
+                        active: 1
+                    }, {
+                    name: "warnning",
+                    active: 1
+                    }]
+            };
+        },
+
+        methods: {
+            gettags : function () {
+                this.items.forEach((item)=>{
+                    tags.push({
+                        name:item.tag,
+                        active: 1
+                    });
+                });
+            },
+            sort : function(criterion) {
+                console.log(this.items[0].tags);
+                this.items.sort((a, b)=> {
+                   if (a[criterion] < b[criterion]) {
+                       return -1;
+                   } else {
+                       return 1;
+                   }
+                });
+            }
         }
     }
 </script>
--- src/js/Api.js
+++ src/js/Api.js
@@ -11,7 +11,6 @@ export default {
         });
     },
 
-
     categories() {
         return this.request('get', 'category');
     },
